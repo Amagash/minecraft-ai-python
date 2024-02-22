b2236d708ed94890f889c92b48619f58a7faef05
Amagash <tiffanysouterre@gmail.com>
2024-02-22 17:18:53 +0100

    add api call to claude v2

--- index.py
+++ index.py
@@ -20,4 +20,15 @@ def handle(this, username, message, *args):
     if username == bot.username:
         return
     else:
-        bot.chat("hi!")
+        bedrock = boto3.client(service_name="bedrock-runtime")
+        body = json.dumps(
+            {
+                "prompt": "\n\nHuman: {} \n\nAssistant:".format(message),
+                "max_tokens_to_sample": 100,
+                "anthropic_version": "bedrock-2023-05-31"
+            }
+        )
+        response = bedrock.invoke_model(body=body, modelId="anthropic.claude-v2:1")
+        response_body = json.loads(response.get("body").read())
+        response = response_body.get("completion")
+        bot.chat(response)

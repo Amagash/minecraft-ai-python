dd0a8f4808d9e899f8889b8b94946fa59b860cc0
Amagash <tiffanysouterre@gmail.com>
2024-05-12 00:26:07 +0200

    update index

--- index.py
+++ index.py
@@ -1,15 +1,21 @@
 import boto3
 import json
 import logging
+import minecraft_data
 from javascript import require, On
 from context.advanced import prompt
 from helper.text_handler import *
+from vec3 import *
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 mineflayer = require('mineflayer')
 pathfinder = require('mineflayer-pathfinder')
+block = require('prismarine-block')
+item = require('prismarine-item')
+windows = require('prismarine-windows')
+recipe = require('prismarine-recipe')
 mineflayerViewer = require('prismarine-viewer')
 inventoryViewer = require('mineflayer-web-inventory')
 pvp = require('mineflayer-pvp')
@@ -23,6 +29,7 @@ bot = mineflayer.createBot({
 
 bot.loadPlugin(pathfinder.pathfinder)
 mcData = require('minecraft-data')(bot.version)
+mcd = minecraft_data(bot.version)
 
 @On(bot, 'spawn')
 def spawn(*args):
@@ -34,37 +41,31 @@ def handle(this, player_name, message, *args):
     if player_name == bot.username:
         return
     else:
-        # bot.chat("hello")
-        # code = """bot.pathfinder.setGoal(pathfinder.goals.GoalNear(0, 0, -1))"""
-        # eval("{}".format(code))
         response = generate_text(message)
         code = extract_substring(response, "<code>", "</code>")
         reasoning = extract_substring(response, "<scratchpad>", "</scratchpad>")
         logger.info("REASONING BEGINNING: %s REASONING END", reasoning)
-        # print(player)
+
+        # dirt_block = bot.findBlock({
+        #   matching: mcdata.blocks.dirt.id,
+        #   maxDistance: 128
+        # })
+
+        # print(dirt_block)
 
         try:
             # WARNING: this is a very dangerous way to execute code! Do you trust AI?
             # Note: the code is executed in the context of the bot entity
-            # print(f"Running: {code}")
-            
             logger.info("CODE BEGINNING: %s CODE END", code)
             exec("{}".format(code))
-            
-            
-            # if code != "":
-            #     code = code.strip()
-            #     n=1
-            #     for code_line in code.split('\n'):
-            #         logger.info("CODE LINE %s BEGINNING: %s CODE END", n, code_line)
-            #         eval("{}".format(code_line))
-            #         n+=1
+
         except Exception as error:
             # print("error: {}".format(error))
             # bot.chat("I could not execute that: {}".format(code))
             # message_with_error = f"I got the following error: {error} with the following code {code} solve the issue."
             logger.info("ERROR BEGINNING: %s ERROR END", error)
-            # new_code = generate_text(message_with_error)
+            bot.chat("Sorry I did not understand, can you repeat that ?")
+            # new_code = generate_text(message)
             # handle(this, player_name, new_code)
 
 

2fdc068a39d55b6e993ee2c573520d0498c326f6
Amagash <tiffanysouterre@gmail.com>
2024-05-12 04:42:55 +0200

    add the concept of digging

--- context/advanced.py
+++ context/advanced.py
@@ -74,10 +74,8 @@ Look at me
 </message>
 <scratchpad>
 The player wants me to look at them. Here are the steps I need to do:
-I need to use the bot.lookAt() method to look at the player's face position. 
-However, I don't have the player's face position. 
 Step1: Get the player's position with bot.players[player_name].entity.position
-Step2: Set the hight of the look to match the player's height by adding 1 to the y position
+Step2: Set the hight of the look to match the player's height by adding 1 to the y coordinate
 Step3: Look at the player using the bot.lookAt() method.
 I have access to the player's name with the player_name argument
 </scratchpad>
@@ -109,25 +107,77 @@ bot.chat("On my way!")
 </code>
 
 <message>
+Dig
+</message>
+<scratchpad>
+The player wants me to dig. Here are the steps I need to do:
+Step1: Find a block to dig. Since we are in creative mode, I'll assume we are digging a block in front of the bot.
+Step2: Dig the block using the bot.dig() method.
+Step3: Respond in the chat with the bot.chat() method.
+</scratchpad>
+<code>
+# Step1: Find a block to dig
+block_to_dig = bot.blockAt(bot.entity.position.offset(0, -1, -1))
+# Step2: Dig the block
+bot.dig(block_to_dig)
+# Step3: Respond in the chat with the bot.chat() method.
+bot.chat("I'm digging!")
+</code>
+
+<message>
+Dig a hole
+</message>
+<scratchpad>
+The player wants me to dig a hole. Here are the steps I need to do:
+Step1: Find a suitable location to dig a hole.
+Step2: Define the dimensions of the hole.
+Step3: Dig the blocks within the defined area using the bot.dig() method.
+Step4: Respond in the chat with the bot.chat() method.
+</scratchpad>
+<code>
+# Step1: Find a suitable location to dig a hole
+hole_location = bot.entity.position.offset(0, -1, 0)
+pos_x = math.floor(hole_location.x)
+pos_y = math.floor(hole_location.y)
+pos_z = math.floor(hole_location.z)
+# Step2: Define the dimensions of the hole
+hole_width = 3
+hole_depth = 3
+hole_height = 1
+# Step3: Dig the blocks within the defined area
+for x in range(pos_x, pos_x + hole_width):
+    for z in range(pos_z, pos_z + hole_depth):
+        for y in range(pos_y, pos_y - hole_height, -1):
+            vector = vec3(x, y, z)
+            block = bot.blockAt(vector)
+            bot.dig(block)
+# Step4: Respond in the chat with the bot.chat() method.
+bot.chat("I've dug a hole!")
+</code>
+
+
+
+<message>
 Get a block of dirt
 </message>
 <scratchpad>
 The player wants me to get a block of dirt. Here are the steps I need to do:
-Step1: Find a block of dirt using the bot.findBlock() method.
-Step2: Dig the block of dirt using the bot.dig() method.
-Step3: Respond in the chat with the bot.chat() method.
+Step1: Find the id of the block of dirt
+Step2: Find a block of dirt using the bot.findBlock() method.
+Step3: Dig the block of dirt using the bot.dig() method.
+Step4: Respond in the chat with the bot.chat() method.
 </scratchpad>
 <code>
-# Step1: Find a block of dirt
-dirt_block = bot.findBlock({
-  matching: mcdata.blocks.dirt.id,
-  maxDistance: 128
-})
-
-# Step2: Dig the block of dirt
-bot.dig(dirt_block)
-
-# Step3: Respond in the chat with the bot.chat() method.
+# Step1: Find the id of the block of dirt
+block_id = mcd.find_item_or_block('dirt').get('id')
+# Step2: Find a block of dirt
+found_block = bot.findBlocks({{
+  "matching": block_id,
+  "maxDistance": 128
+}})
+# Step3: Dig the block of dirt
+bot.dig(bot.blockAt(found_block[0]))
+# Step4: Respond in the chat with the bot.chat() method.
 bot.chat("Got a block of dirt!")
 </code>
 

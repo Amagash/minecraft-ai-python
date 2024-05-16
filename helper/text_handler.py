e6aee1bd34ea36a7e8a708a7e08dfeeacb360f48
Amagash <tiffanysouterre@gmail.com>
2024-05-16 00:48:16 -0700

    clean text_handler

--- helper/text_handler.py
+++ helper/text_handler.py
@@ -1,17 +1,16 @@
 import boto3
 import json
 import logging
-from context.advanced import prompt
+# from context.advanced import prompt
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
-
-def generate_text(message):
+def generate_text(message, prompt):
     model_id = "mistral.mistral-large-2402-v1:0"
     message_prompt = prompt.replace("[[MESSAGE]]", message)
+    # print(message_prompt)
     prompt_to_ai = """<s>[INST] {} [/INST]""".format(message_prompt)
-    # logger.info("PROMPT BEGINNING: %s PROMPT ENDS", prompt_to_ai)
-
+    # print(prompt_to_ai)
     bedrock = boto3.client(service_name="bedrock-runtime")
 
     body = json.dumps(
@@ -23,17 +22,20 @@ def generate_text(message):
             "top_k": 50
         }
     )
+    logger.info("PROMPT BEGINNING: %s PROMPT END", prompt_to_ai)
 
-    bedrock = boto3.client(service_name='bedrock-runtime')
 
     response = bedrock.invoke_model(
         body=body,
         modelId=model_id
     )
-
+    # print(response)
     response_body = json.loads(response.get("body").read())
+    # print(response_body)
     outputs = response_body.get('outputs')
+    # print("OUTPUTS: ", outputs)
     text = outputs[0]['text']
+    # print("TEXT: ", text)
     return text
 
 def extract_substring(text, trigger_str, end_str):
